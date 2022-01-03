180a34c3d9a2110a0cad365e4721a8835f5c9154
tusiaa <sandra.leman146@gmail.com>
2022-01-03 22:44:24 +0100

    TDD - Zamowienie add remove item functions

--- src/zamowienie.py
+++ src/zamowienie.py
@@ -10,3 +10,17 @@ class Zamowienie:
         self.id = id
         self.klient_id = klient_id
         self.przedmioty = []
+
+    def dodaj_przedmiot(self, przedmiot_id: int):
+        if type(przedmiot_id) is not int:
+            raise ValueError("przedmiot_id nie jest liczba")
+        if not Baza_Danych.znajdz_przedmiot(przedmiot_id):
+            raise ValueError("Nie ma takiego przedmiotu")
+        self.przedmioty.append(przedmiot_id)
+        Baza_Danych().dodaj_przedmiot_do_zamowienia(self.id, przedmiot_id)
+
+    def usun_przedmiot(self, przedmiot_id: int):
+        if type(przedmiot_id) is not int:
+            raise ValueError("przedmiot_id nie jest liczba")
+        self.przedmioty.remove(przedmiot_id)
+        Baza_Danych().usun_przedmiot_z_zamowienia(self.id, przedmiot_id)
