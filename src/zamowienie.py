45e44c3d57b27dcfd116f39b29b727be6257c9de
tusiaa <sandra.leman146@gmail.com>
2022-01-03 22:03:41 +0100

    TDD - Zamowienie init errors

--- src/zamowienie.py
+++ src/zamowienie.py
@@ -2,7 +2,11 @@ from src.baza_danych import Baza_Danych
 from src.przedmiot import Przedmiot
 
 class Zamowienie:
-    def __init__(self, id, klient_id):
+    def __init__(self, id: int, klient_id: int):
+        if type(id) is not int:
+            raise ValueError("id nie jest liczba")
+        if type(klient_id) is not int:
+            raise ValueError("klient_id nie jest liczba")
         self.id = id
         self.klient_id = klient_id
         self.przedmioty = []
--- tests/tests_zamowienie.py
+++ tests/tests_zamowienie.py
@@ -8,7 +8,7 @@ class TestsZamowienie(unittest.TestCase):
     @patch.object(Baza_Danych, 'czytaj_zamowienia', return_value=[(1, 11)])
     @patch.object(Baza_Danych, 'znajdz_przedmioty_z_zamowienia', return_value=[(1, 111)])
     @patch.object(Baza_Danych, 'znajdz_przedmiot', return_value=(111, "Nazwa", 100.0))
-    def setUp(self, mock_czytaj_zamowienia, mock_znajdz_przedmioty_z_zamowienia, mock_znajdz_przedmiot):
+    def setUp(self, mock_znajdz_przedmiot, mock_znajdz_przedmioty_z_zamowienia, mock_czytaj_zamowienia):
         self.zamowienie = Zamowienie(mock_czytaj_zamowienia()[0][0], mock_czytaj_zamowienia()[0][1])
         przedmiot = mock_znajdz_przedmiot(mock_znajdz_przedmioty_z_zamowienia(mock_czytaj_zamowienia()[0][0])[0][1])
         self.zamowienie.przedmioty.append(Przedmiot(przedmiot[0], przedmiot[1], przedmiot[2]))
