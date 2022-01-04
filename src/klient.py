45b09db401496dfc4b788cca076d6447f85f0764
tusiaa <sandra.leman146@gmail.com>
2022-01-04 01:31:55 +0100

    TDD - Klient init errors

--- src/klient.py
+++ src/klient.py
@@ -3,6 +3,14 @@ from src.zamowienie import Zamowienie
 
 class Klient:
     def __init__(self, id, imie, nazwisko, email):
+        if type(id) is not int:
+            raise ValueError("id nie jest liczba")
+        if type(imie) is not str or not imie:
+            raise ValueError("imie nie jest stringiem")
+        if type(nazwisko) is not str or not nazwisko:
+            raise ValueError("nazwisko nie jest stringiem")
+        if type(email) is not str or not email:
+            raise ValueError("email nie jest stringiem")
         self.id = id
         self.imie = imie
         self.nazwisko = nazwisko
