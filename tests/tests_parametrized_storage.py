733531e9ddbc1422ebac8391d1b8beb5d44efba1
tusiaa <sandra.leman146@gmail.com>
2022-01-05 11:27:40 +0100

    TDD - Storage init test

--- tests/tests_parametrized_storage.py
+++ tests/tests_parametrized_storage.py
@@ -4,9 +4,10 @@ from parameterized import *
 from unittest.mock import *
 from src.storage import *
 
-@parameterized_class(('str_wrong_value', 'int_wrong_value', 'positive_float_wrong_value'), [
-    (1, "int", -5.0),
-    (1.5, 1.5, "float"),
+@parameterized_class(('str_wrong_value', 'int_wrong_value', 'class_wrong_value'), [
+    (1, "int", 5.0),
+    (1, "int", 5),
+    (1.5, 1.5, "class"),
     (True, True, True),
     (None, None, None),
     ("", "", ""),
@@ -14,5 +15,14 @@ from src.storage import *
     ({'name': 2, 'grades': 4}, {'name': 2, 'grades': 4}, {'name': 2, 'grades': 4}),
 ])
 class TestsParametrizedStorage(unittest.TestCase):
-    def setUp(self):
+
+    @patch.object(Baza_Danych, 'czytaj_klientow', return_value=[(11, "Jan", "Kowalski", "mail")])
+    @patch.object(Baza_Danych, 'czytaj_zamowienia', return_value=[(1, 11)])
+    @patch.object(Baza_Danych, 'czytaj_przedmioty_z_zamowien', return_value=[(1, 111)])
+    @patch.object(Baza_Danych, 'czytaj_przedmioty', return_value=[(111, "Nazwa", 100.0)])
+    def setUp(self, mock_czytaj_przedmioty, mock_czytaj_przedmioty_z_zamowien, mock_czytaj_zamowienia, mock_czytaj_klientow):
         self.storage = Storage()
+        self.storage.klienci.append(mock_czytaj_klientow()[0])
+        self.storage.zamowienia.append(mock_czytaj_zamowienia()[0])
+        self.storage.przedmioty.append(mock_czytaj_przedmioty()[0])
+        self.storage.przedmioty_w_zamowieniach.append(mock_czytaj_przedmioty_z_zamowien()[0])
--- tests/tests_storage.py
+++ tests/tests_storage.py
@@ -4,5 +4,17 @@ from unittest.mock import *
 from src.storage import *
 
 class TestsStorage(unittest.TestCase):
-    def setUp(self):
+
+    @patch.object(Baza_Danych, 'czytaj_klientow', return_value=[(11, "Jan", "Kowalski", "mail")])
+    @patch.object(Baza_Danych, 'czytaj_zamowienia', return_value=[(1, 11)])
+    @patch.object(Baza_Danych, 'czytaj_przedmioty_z_zamowien', return_value=[(1, 111)])
+    @patch.object(Baza_Danych, 'czytaj_przedmioty', return_value=[(111, "Nazwa", 100.0)])
+    def setUp(self, mock_czytaj_przedmioty, mock_czytaj_przedmioty_z_zamowien, mock_czytaj_zamowienia, mock_czytaj_klientow):
         self.storage = Storage()
+        self.storage.klienci.append(mock_czytaj_klientow()[0])
+        self.storage.zamowienia.append(mock_czytaj_zamowienia()[0])
+        self.storage.przedmioty.append(mock_czytaj_przedmioty()[0])
+        self.storage.przedmioty_w_zamowieniach.append(mock_czytaj_przedmioty_z_zamowien()[0])
+
+    def test_storage_init(self):
+        assert_that(self.storage).is_not_none()
