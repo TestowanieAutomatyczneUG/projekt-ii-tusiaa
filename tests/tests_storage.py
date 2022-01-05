01e244b2d1abc2fc861ac155be13ccd3e43522bc
tusiaa <sandra.leman146@gmail.com>
2022-01-05 11:36:47 +0100

    Added classes instances

--- tests/tests_storage.py
+++ tests/tests_storage.py
@@ -11,9 +11,9 @@ class TestsStorage(unittest.TestCase):
     @patch.object(Baza_Danych, 'czytaj_przedmioty', return_value=[(111, "Nazwa", 100.0)])
     def setUp(self, mock_czytaj_przedmioty, mock_czytaj_przedmioty_z_zamowien, mock_czytaj_zamowienia, mock_czytaj_klientow):
         self.storage = Storage()
-        self.storage.klienci.append(mock_czytaj_klientow()[0])
-        self.storage.zamowienia.append(mock_czytaj_zamowienia()[0])
-        self.storage.przedmioty.append(mock_czytaj_przedmioty()[0])
+        self.storage.klienci.append(Klient(mock_czytaj_klientow()[0]))
+        self.storage.zamowienia.append(Zamowienie(mock_czytaj_zamowienia()[0]))
+        self.storage.przedmioty.append(Przedmiot(mock_czytaj_przedmioty()[0]))
         self.storage.przedmioty_w_zamowieniach.append(mock_czytaj_przedmioty_z_zamowien()[0])
 
     def test_storage_init(self):
